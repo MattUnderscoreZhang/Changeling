8dd2ce880d8e213030cf3ba997b66e194236244f
Matt Zhang <set.stun@gmail.com>
2023-03-25 19:48:22 -0400

    Added tests for learning criteria

---
+++ changeling/tests/test_learning_criteria.py
@@ -0,0 +1,20 @@
+from changeling.core.learning_criteria import NEpochsComplete, AccuracyThresholdAchieved
+
+
+def test_n_epochs_complete():
+    learning_criteria = NEpochsComplete(3)
+    assert learning_criteria(0.5, 0.8) == False
+    assert learning_criteria(0.5, 0.8) == False
+    assert learning_criteria(0.5, 0.8) == True
+    assert learning_criteria(0.5, 0.8) == False
+
+def test_accuracy_threshold_achieved():
+    learning_criteria = AccuracyThresholdAchieved(0.9)
+    assert learning_criteria(0.5, 0.8) == False
+    assert learning_criteria(0.5, 0.85) == False
+    assert learning_criteria(0.5, 0.9) == False
+    assert learning_criteria(0.5, 0.9) == False
+    assert learning_criteria(0.5, 0.9) == True
+    assert learning_criteria(0.5, 0.9) == False
+    assert learning_criteria(0.5, 0.9) == False
+    assert learning_criteria(0.5, 0.9) == True
